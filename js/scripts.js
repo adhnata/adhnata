af843a46da2358f368794d6c184e0cc111a532b9
prometheus <111395246+Ye-Mon-Thant@users.noreply.github.com>
2025-04-17 15:32:37 -0400

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//  Mobile nav toggle
+const navToggle = document.querySelector(".nav‑toggle");
+const siteNav   = document.getElementById("siteNav");
+
+navToggle.addEventListener("click", () => {
+  siteNav.classList.toggle("show");
+  navToggle.classList.toggle("open");
+});
